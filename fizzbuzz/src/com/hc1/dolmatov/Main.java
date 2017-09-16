e62aad33369820c1af2fe20f7b15d6d417fa3139
maximdolmatov <maximdolmatov99@gmail.com>
2017-09-16 21:21:33 +0700

    Модель игры в FizzBizz.

---
+++ fizzbuzz/src/com/hc1/dolmatov/Main.java
@@ -0,0 +1,20 @@
+/*Игра в FizzBuzz*/
+
+package com.hc1.dolmatov;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Введите число от 0 до 100: ");
+
+        for(int i = 1; i < 101; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0))
+                System.out.println("FizzBuzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else System.out.println (i);
+        }
+    }
+}
